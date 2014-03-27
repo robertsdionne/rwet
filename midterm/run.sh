a0877d59df4da3003fe6d9002ce435fd1452b6e4
Robert Dionne <robertsdionne@gmail.com>
2014-03-27 14:54:41 -0400

    Generate folio txt files in addition to folio html files.

--- midterm/run.sh
+++ midterm/run.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 
 source ../venv/bin/activate
+
 echo "processing f1r"
 cat data/L16+h-eva/f1r.P* | ./process_voynich.py | ./chunk_words.py --image f1r.png \
     --html -d cmudict07a.json -s lightinaugust.txt > f1r.html
+cat data/L16+h-eva/f1r.P* | ./process_voynich.py | ./chunk_words.py -d cmudict07a.json \
+    -s lightinaugust.txt > f1r.txt
+
 echo "processing f39v"
 cat data/L16+h-eva/f39v.P* | ./process_voynich.py | ./chunk_words.py --image f39v.png \
     --html -d cmudict07a.json -s rosettastone.txt > f39v.html
+cat data/L16+h-eva/f39v.P* | ./process_voynich.py | ./chunk_words.py -d cmudict07a.json \
+    -s rosettastone.txt > f39v.txt
+
 echo "processing f81r"
 cat data/L16+h-eva/f81r.P* | ./process_voynich.py | ./chunk_words.py --image f81r.png \
     --html -d cmudict07a.json -s odyssey.txt > f81r.html
+cat data/L16+h-eva/f81r.P* | ./process_voynich.py | ./chunk_words.py -d cmudict07a.json \
+    -s odyssey.txt > f81r.txt
