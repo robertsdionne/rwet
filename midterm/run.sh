743cc9df498d22759c9545f720c6b03d7b172cc2
Robert Dionne <robertsdionne@gmail.com>
2014-03-27 15:40:11 -0400

    Add descriptive names too.

--- midterm/run.sh
+++ midterm/run.sh
@@ -15,28 +15,28 @@ mkdir -p temp
 ./cleanup_text.py < text/pg1727.txt > temp/odyssey.txt
 
 
-echo "generating f1r.html"
+echo "generating f1r_lightinaugust.html"
 cat text/L16+h-eva/f1r.P* | ./process_voynich.py | ./translate.py --image image/f1r.png \
-    --html -d temp/cmudict07a.json -s temp/lightinaugust.txt > f1r.html
+    --html -d temp/cmudict07a.json -s temp/lightinaugust.txt > f1r_lightinaugust.html
 
-echo "generating f1r.txt"
+echo "generating f1r_lightinaugust.txt"
 cat text/L16+h-eva/f1r.P* | ./process_voynich.py | ./translate.py -d temp/cmudict07a.json \
-    -s temp/lightinaugust.txt > f1r.txt
+    -s temp/lightinaugust.txt > f1r_lightinaugust.txt
 
 
-echo "generating f39v.html"
+echo "generating f39v_rosettastone.html"
 cat text/L16+h-eva/f39v.P* | ./process_voynich.py | ./translate.py --image image/f39v.png \
-    --html -d temp/cmudict07a.json -s temp/rosettastone.txt > f39v.html
+    --html -d temp/cmudict07a.json -s temp/rosettastone.txt > f39v_rosettastone.html
 
-echo "generating f39v.txt"
+echo "generating f39v_rosettastone.txt"
 cat text/L16+h-eva/f39v.P* | ./process_voynich.py | ./translate.py -d temp/cmudict07a.json \
-    -s temp/rosettastone.txt > f39v.txt
+    -s temp/rosettastone.txt > f39v_rosettastone.txt
 
 
-echo "generating f81r.html"
+echo "generating f81r_odyssey.html"
 cat text/L16+h-eva/f81r.P* | ./process_voynich.py | ./translate.py --image image/f81r.png \
-    --html -d temp/cmudict07a.json -s temp/odyssey.txt > f81r.html
+    --html -d temp/cmudict07a.json -s temp/odyssey.txt > f81r_odyssey.html
 
-echo "generating f81r.txt"
+echo "generating f81r_odyssey.txt"
 cat text/L16+h-eva/f81r.P* | ./process_voynich.py | ./translate.py -d temp/cmudict07a.json \
-    -s temp/odyssey.txt > f81r.txt
+    -s temp/odyssey.txt > f81r_odyssey.txt
