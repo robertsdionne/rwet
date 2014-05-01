4918ab73cc4006255f06fb3115ba1271148be01f
Robert Dionne <robertsdionne@gmail.com>
2014-05-01 13:13:26 -0400

    Update index.

--- final/make_index.sh
+++ final/make_index.sh
@@ -3,7 +3,7 @@ echo '<body>'
 echo '<p>'
 for item in $(ls reading*.html); do
   first=$(grep '<p>' $item | cut -d '>' -f 2 | cut -d '<' -f 1)
-  echo "<a href=\"$item\">$first</a><br />"
+  echo "<a href=\"$item\">$item: $first</a><br />"
 done
 echo '</p>'
 echo '</body>'
